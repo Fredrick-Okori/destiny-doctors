5f49476d1f7281a1de704cff3aa6eea854cb6cbc
Excitement <graphics@Graphicss-MacBook-Pro.local>
2024-01-19 20:43:49 +0300

    Corrected Mailing

--- components/handleMail.jsx
+++ components/handleMail.jsx
@@ -4,9 +4,9 @@ import { Button } from '@chakra-ui/react';
 
 const ContactButton = () => {
     const handleButtonClick = () => {
-        const email = 'your@email.com'; // Replace with your email address
-        const subject = 'Subject of the email'; // Replace with your desired subject
-        const body = 'Hello, I would like to get in touch with you.'; // Replace with your desired email body
+        const email = 'destinydoctors1@gmail.com'; 
+        const subject = 'Partnerships'; 
+        const body = 'Hello, I love your work. Can we schedule a meeting?'; // Replace with your desired email body
 
         const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 
