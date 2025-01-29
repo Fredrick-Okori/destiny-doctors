2a2534b77a7126c7ccb96d73707ee761bcc356cc
Goddie-Owiny <goddiegodfreyz07@gmail.com>
2025-01-29 21:01:58 +0300

    Adding Assistant to website

--- app/layout.js
+++ app/layout.js
@@ -2,9 +2,9 @@
 import { useEffect, useState } from 'react';
 import NavBar from '@/components/navbar';
 import Footer from '@/components/footer';
-
 import { Lexend } from 'next/font/google'
 import { Providers } from './providers';
+import Script from 'next/script';
 
 const lexend = Lexend({
   style: ['normal'],
@@ -15,15 +15,16 @@ const lexend = Lexend({
 export default function RootLayout(props) {
   const { children } = props
   return (
-    <html suppressHydrationWarning> {/* Apply the Lexend font class */}
+    <html suppressHydrationWarning> 
       <body className={lexend.className}>
         <Providers>
           <NavBar/>
           {children}
           <Footer/>
         </Providers>
+        
         <chat-widget env="prod" project-id="d2f79889-b34b-4e1c-8952-51931e5c9871"></chat-widget>
-        <script src="https://cdn.apollo.kayeai.com/js/chat-widget.js" type="module"></script>
+        <Script src="https://cdn.apollo.kayeai.com/js/chat-widget.js" type="module" async></Script>
       </body>
     </html>
   )
